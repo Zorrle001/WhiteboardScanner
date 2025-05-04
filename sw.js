151d9687e2b1478427ef792e5199f187b4270215
Simon M <smmaecht@gmail.com>
2025-05-04 04:36:43 +0200

    Added Notification Icon

--- sw.js
+++ sw.js
@@ -102,6 +102,7 @@ self.addEventListener("push", (event) => {
     event.waitUntil(
         self.registration.showNotification("WhiteboardScanner", {
             body: payload,
+            icon: "icons/PushShare.png",
         })
     );
 });
